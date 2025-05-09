1b19406705a3ae04301e4d5908433a40bcb6827b
leesoli0122 <leesoli0122@gmail.com>
2025-05-09 10:04:21 +0900

    디자인 적용 및 css 정리 같이 하는 중

--- IM/assets/js/class.js
+++ IM/assets/js/class.js
@@ -1,2 +1,10 @@
-const $tabWrap = $('.search_wrap').next('.tab_wrap');
-$tabWrap.find('.cont_in_tab').remove();
+$(document).ready(function() {
+	$('.move_tbl .check_stl').on('change', function() {
+		const $tr = $(this).closest('tr');
+		if ($(this).is(':checked')) {
+			$tr.addClass('checked');
+		} else {
+			$tr.removeClass('checked');
+		}
+	});
+});
