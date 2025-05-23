99c5cbccd6808a0f89f6c9b6e2bfab85affda702
leesoli0122 <leesoli0122@gmail.com>
2025-05-23 10:02:13 +0900

    모니터링하는 중

--- IM/js/gauge.js
+++ IM/js/gauge.js
@@ -1,6 +1,19 @@
+/*---------------------------------------------
+  수정 : lineWidth,colorStart,colorStop,strokeColor
+  추가 : gauge_box에 bg-yel 클래스가 추가될 때 -> 추가라고 적어둠
+---------------------------------------------*/
+// 공통 색상 결정 함수 -> 추가
+function getGaugeColor(canvasId) {
+  var canvas = document.getElementById(canvasId);
+  var gaugeBox = canvas.closest('.gauge_box');
+  var hasYellowBg = gaugeBox.classList.contains('bg-yel');
+  return hasYellowBg ? '#F76900' : '#27AD98';
+}
+
+var gaugeColor01 = getGaugeColor('view-gauge'); //추가
 var opts = {
   angle: 0.25, // The span of the gauge arc
-  lineWidth: 0.09, // The line thickness
+  lineWidth: 0.07, // The line thickness
   radiusScale: 1, // Relative radius
   pointer: {
     length: 0.6, // // Relative to gauge radius
@@ -9,9 +22,9 @@ var opts = {
   },
   limitMax: false,     // If false, max value increases automatically if value > maxValue
   limitMin: false,     // If true, the min value of the gauge will be fixed
-  colorStart: '#4cb5f8',   // Colors
-  colorStop: '#4cb5f8',    // just experiment with them
-  strokeColor: '#e9ecf0',  // to see which ones work best for you
+  colorStart: gaugeColor01,   // Colors
+  colorStop: gaugeColor01,    // just experiment with them
+  strokeColor: '#7388A924',  // to see which ones work best for you
   generateGradient: true,
   highDpiSupport: true,     // High resolution support
   
@@ -24,10 +37,10 @@ gauge.animationSpeed = 20; // set animation speed (32 is default value)
 gauge.set(50); // set actual value
 
 
-
+var gaugeColor02 = getGaugeColor('view-gauge02');//추가
 var opts02 = {
   angle: 0.25, // The span of the gauge arc
-  lineWidth: 0.09, // The line thickness
+  lineWidth: 0.07, // The line thickness
   radiusScale: 1, // Relative radius
   pointer: {
     length: 0.6, // // Relative to gauge radius
@@ -36,9 +49,9 @@ var opts02 = {
   },
   limitMax: false,     // If false, max value increases automatically if value > maxValue
   limitMin: false,     // If true, the min value of the gauge will be fixed
-  colorStart: '#4cb5f8',   // Colors
-  colorStop: '#4cb5f8',    // just experiment with them
-  strokeColor: '#e9ecf0',  // to see which ones work best for you
+  colorStart: gaugeColor02,   // Colors
+  colorStop: gaugeColor02,    // just experiment with them
+  strokeColor: '#7388A924',  // to see which ones work best for you
   generateGradient: true,
   highDpiSupport: true,     // High resolution support
   
@@ -51,9 +64,10 @@ gauge02.animationSpeed = 20; // set animation speed (32 is default value)
 gauge02.set(12); // set actual value
 
 
+var gaugeColor03 = getGaugeColor('view-gauge03');//추가
 var opts03 = {
   angle: 0.25, // The span of the gauge arc
-  lineWidth: 0.09, // The line thickness
+  lineWidth: 0.07, // The line thickness
   radiusScale: 1, // Relative radius
   pointer: {
     length: 0.6, // // Relative to gauge radius
@@ -62,9 +76,9 @@ var opts03 = {
   },
   limitMax: false,     // If false, max value increases automatically if value > maxValue
   limitMin: false,     // If true, the min value of the gauge will be fixed
-  colorStart: '#fbb04e',   // Colors
-  colorStop: '#fbb04e',    // just experiment with them
-  strokeColor: '#e9ecf0',  // to see which ones work best for you
+  colorStart: gaugeColor03,   // Colors
+  colorStop: gaugeColor03,    // just experiment with them
+  strokeColor: '#7388A924',  // to see which ones work best for you
   generateGradient: true,
   highDpiSupport: true,     // High resolution support
   
